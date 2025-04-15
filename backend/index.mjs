8f4e80de3887214e39a7ac3c172b5da2b36f2652
Maria Shakeel <154729621+MariaShakeel10@users.noreply.github.com>
2025-04-15 22:50:41 +0500

    Update index.mjs

--- backend/index.mjs
+++ backend/index.mjs
@@ -15,7 +15,7 @@ app.use(
 	cors({
 		origin: ['http://localhost:5174',
 			 'http://localhost:5173',
-			"https://mern-hosting-practice-frontend.vercel.app/"
+			"https://mern-hosting-practice-frontend.vercel.app"
 			
 			],
 		methods: ['GET', 'PUT', 'POST', 'DELETE'],
